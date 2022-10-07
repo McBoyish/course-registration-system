bd54f13171dd97561a7c89e8c6d4ccb316f5c3bd
jake <jake@eventdesk.co>
2022-10-07 15:53:58 -0400

    early exit on error

--- create-course.php
+++ create-course.php
@@ -1,14 +1,14 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
+
 // values for day and semester
 // day: "mon", "tue", "wed", "thu", "fri", "sat", "sun"
-// days: day-day-day...
+// days: day-day-day...ie "mon-thu"
 // semester: "summer", "winter", "fall"
 
 // courseTime can be "hh:mm:ss" or "hh:mm"
 // start and end date should be "yyyy-mm-dd"
-
 $courseCode = $_POST["courseCode"];
 $title = $_POST["title"];
 $semester = $_POST["semester"];
@@ -37,18 +37,25 @@ function json($message, $code)
 $insertCourse = "INSERT INTO Course (courseCode, title, semester, days, courseTime, instructor, room, startDate, endDate)
 			     VALUES ('$coursecode', '$title','$semester','$days', '$courseTime', '$instructor', '$room', '$startDate', '$endDate')";
 
-// Connect to MySQL
-if (!($database = mysqli_connect("localhost", "root", "")))
+if (!($database = mysqli_connect("localhost", "root", ""))) {
   echo json("Internal server error", 500);
+  exit;
+}
 
-if (!mysqli_select_db($database, "Assignment1"))
+if (!mysqli_select_db($database, "Assignment1")) {
   echo json("Internal server error", 500);
+  exit;
+}
 
-if (courseExists($database, $courseCode))
+if (courseExists($database, $courseCode)) {
   echo json("Course code already exists", 400);
+  exit;
+}
+
 
 if (!($insertUserResult = mysqli_query($database, $insertCourse))) {
   echo json("Internal server error", 500);
+  exit;
 }
 
 mysqli_close($database);
